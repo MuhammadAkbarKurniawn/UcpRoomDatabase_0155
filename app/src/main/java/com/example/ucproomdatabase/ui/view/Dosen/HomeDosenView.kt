de29af0992d064828cb0cc6427166019c2884f4b
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-12-23 10:29:57 +0700

    Membuat class view homedosen

---
+++ app/src/main/java/com/example/ucproomdatabase/ui/view/Dosen/HomeDosenView.kt
@@ -0,0 +1,223 @@
+package com.example.ucproomdatabase.ui.view.Dosen
+
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucproomdatabase.data.entity.Dosen
+import com.example.ucproomdatabase.repository.RepositoryDosen
+import com.example.ucproomdatabase.ui.customwidget.CustomTopBar
+import com.example.ucproomdatabase.ui.viewmodel.HomeDosenViewModel
+import com.example.ucproomdatabase.ui.viewmodel.HomeUiStateDsn
+import com.example.ucproomdatabase.ui.viewmodel.HomeUiStateMK
+import com.example.ucproomdatabase.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun HomeDosenView(
+    viewModel: HomeDosenViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddDsn: () -> Unit = { },
+    onDetailClick: (String) -> Unit = { },
+    onBack: () -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    Scaffold(
+        topBar = {
+            CustomTopBar(
+                judul = "Daftar Dosen",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
+            )
+
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddDsn,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Data Dosen"
+                )
+            }
+        }
+    ) { innerPadding ->
+        val homeUiStateDsn by viewModel.homeUiStateDosen.collectAsState()
+
+        BodyHomeDosenView(
+            homeUiStateDsn = homeUiStateDsn,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun BodyHomeDosenView(
+    homeUiStateDsn: HomeUiStateDsn,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() } // Snnackbar state
+    when {
+        homeUiStateDsn.isLoading -> {
+            //menampilkan indikator Login
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        homeUiStateDsn.isError -> {
+            //menampilkan pesan eror
+            LaunchedEffect(homeUiStateDsn) {
+                homeUiStateDsn.errorMessage?.let {message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message) //TampilanSnackbar
+                    }
+                }
+            }
+        }
+
+        homeUiStateDsn.listDsn.isEmpty() -> {
+            //menampilkan pesan jika data kosong
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Tidak ada data Dosen.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            //menampilkan daftar dosen
+            ListDosen(
+                listDsn = homeUiStateDsn.listDsn,
+                onClick = {
+                    onClick(it)
+                    println(it)
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
+
+@Composable
+fun ListDosen(
+    listDsn: List<Dosen>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+){
+    LazyColumn (
+        modifier = modifier
+    ){
+        items(
+            items = listDsn,
+            itemContent = { dsn ->
+                CardDsn(
+                    dsn = dsn,
+                    onClick = { onClick(dsn.nidn)}
+                )
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardDsn (
+    dsn: Dosen,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
+) {
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column (
+            modifier = Modifier.padding(8.dp)
+        ){
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = dsn.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.DateRange, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = dsn.nidn,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            }
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Icon(imageVector = Icons.Filled.Home, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = dsn.jenisKelamin,
+                    fontWeight = FontWeight.Bold,
+                )
+            }
+        }
+    }
+}
